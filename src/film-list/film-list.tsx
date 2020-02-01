046472ecefbd8ffb80fcbe2e08924570f2f2f2ee
a.kintsel <a.kintsel@movavi.com>
2020-02-01 22:47:28 +0700

    fix after review

--- src/film-list/film-list.tsx
+++ src/film-list/film-list.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./film-list.css";
-import { FilmItem } from "../film-item/film-item";
+import { FilmItem, FilmsItemType } from "../film-item/film-item";
 
 export const FilmList = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<FilmsItemType[]>([]);
+  const api = "https://devlab.website/v1/movies";
 
   useEffect(() => {
-    fetch("https://devlab.website/v1/movies")
+    fetch(api)
       .then(response => response.json())
       .then(setData);
   }, []);
 
   return (
     <div className="film-list">
-      {data.map((film: any) => (
+      {data.map(film => (
         <FilmItem {...film} key={film.id} />
       ))}
     </div>
--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./header.css";
 
-export const Header = () => {
-  return (
-    <header className="header">
-      <div className="header-wrapper">
-        <div className="logo">Pino Koisk</div>
-      </div>
-    </header>
-  );
-};
+export const Header = () => (
+  <header className="header">
+    <div className="header-wrapper">
+      <div className="logo">Pino Koisk</div>
+    </div>
+  </header>
+);
